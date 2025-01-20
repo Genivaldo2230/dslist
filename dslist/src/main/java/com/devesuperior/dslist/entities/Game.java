7640213e283c108f9c5e62f7a02415cc228a0290
Genivaldo2230 <gda_anjos@hotmail.com.br>
2025-01-20 12:22:06 -0300

    Add Game,inform

--- dslist/src/main/java/com/devesuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devesuperior/dslist/entities/Game.java
@@ -13,12 +13,17 @@ public class Game {
     private long id;
     private String title;
 
-    @Column(name = "game_yaer")
+    @Column(name = "game_year")
     private int year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -26,17 +31,18 @@ public class Game {
 
     }
 
-
-    public Game(long id, String title, int year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(long id, String title, int year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
+
 
     public long getId() {
         return id;
@@ -76,6 +82,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
