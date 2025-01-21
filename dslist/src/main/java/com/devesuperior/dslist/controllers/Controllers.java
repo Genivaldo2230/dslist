57117a5ae450829c380d955977f7e6cc391f8ed0
Genivaldo2230 <gda_anjos@hotmail.com.br>
2025-01-21 15:38:17 -0300

    Get game by id

--- dslist/src/main/java/com/devesuperior/dslist/controllers/Controllers.java
+++ dslist/src/main/java/com/devesuperior/dslist/controllers/Controllers.java
@@ -1,9 +1,11 @@
 package com.devesuperior.dslist.controllers;
 
+import com.devesuperior.dslist.dto.GameDTO;
 import com.devesuperior.dslist.dto.GameMiniDTO;
 import com.devesuperior.dslist.services.GameServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +18,22 @@ public class Controllers {
     @Autowired
     private GameServices gameServices;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findAll(@PathVariable Long id) {
+        GameDTO result = gameServices.findById(id);
+        return result;
+
+
+    }
+
     @GetMapping
     public List<GameMiniDTO> findAll() {
         List<GameMiniDTO> result = gameServices.findAll();
         return result;
 
     }
+
+
 
 
 }
--- dslist/src/main/java/com/devesuperior/dslist/services/GameServices.java
+++ dslist/src/main/java/com/devesuperior/dslist/services/GameServices.java
@@ -1,20 +1,31 @@
 package com.devesuperior.dslist.services;
 
+import com.devesuperior.dslist.dto.GameDTO;
 import com.devesuperior.dslist.dto.GameMiniDTO;
 import com.devesuperior.dslist.entities.Game;
 import com.devesuperior.dslist.repositorie.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component //registrando componente de componetes no sistemas
 public class GameServices {
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+
+        return dto;
+    }
+
     @Autowired
     private GameRepository gameRepository; //Aqui estamos injetando estancia do gameRepositry sistema
 
-
+    @Transactional(readOnly = true)
     public List<GameMiniDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMiniDTO> dto = result.stream().map(x -> new GameMiniDTO(x)).toList();
